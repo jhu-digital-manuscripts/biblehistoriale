87793219fe674a2efc26993f2cc1c631f4480208
jabrah <jabrah23@gmail.com>
2013-08-12 16:15:02 -0400

    Added skeleton Maven modules for website, model, search, profile builder

---
+++ biblehistoriale-website/src/main/java/edu/jhu/library/biblehistoriale/website/client/BibleHistorialeWebsite.java
@@ -0,0 +1,15 @@
+package edu.jhu.library.biblehistoriale.website.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class BibleHistorialeWebsite implements EntryPoint {
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+
+	}
+}
